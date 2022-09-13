451a842b3ce75f68b73f2f0c139ba13a907a000c
ben99933 <52560120+ben99933@users.noreply.github.com>
2022-09-13 13:43:25 +0800

    Update Main.java

--- C_AR02/Main.java
+++ C_AR02/Main.java
@@ -2,22 +2,27 @@ import java.util.Scanner;
 
 public class Main {
 
+	/* 
+	 * 題目規定輸入六個數字(固定6個) 然後以相反的順序輸出
+	 * 那我就先反著將數字存入陣列
+	 * 輸出的時候順著輸出就好
+	 */
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		while (input.hasNext()) {
 			long array[] = new long[6];
+			//從index=5到index=0反向指派
 			for (int index = 5; index >= 0; index--) {
 				array[index] = input.nextLong();
 			}
-			boolean flag = false;
+			boolean space = false;//控制是否要print空白
 			for (int index = 0; index < 6; index++) {
-				if(flag)System.out.print(" ");
-				else flag = true;
+				if(space)System.out.print(" ");
+				else space = true;
 				System.out.print(array[index]);
 			}
 			System.out.println();
 		}
 		input.close();
 	}
-
 }
