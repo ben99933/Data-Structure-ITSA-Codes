479e5f31e328fae70402d2db114e2c595aed4bc8
ben99933 <yangben999@gmail.com>
2023-01-08 21:36:16 +0800

    test

--- README.md
+++ README.md
@@ -13,3 +13,31 @@ p15910 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?a=15910
 p30752 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30752
 
 p30757 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30757
+
+p30787 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30787
+
+p30216 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30216
+
+p7907 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30218
+
+p3087 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=3087
+
+p15139 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=15139
+
+p30211 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30211
+
+p6681 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6861
+
+p6867 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6867
+
+p6873 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6873
+
+p6879 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6879
+
+p6885 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6885
+
+p6894 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6894
+
+p6903 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6903
+
+p6906 https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6906
---
+++ p30787.c
@@ -0,0 +1,268 @@
+#include<stdio.h>  
+#include<stdlib.h>  
+#include<stdbool.h>  
+#include<string.h>  
+#include<assert.h>  
+#include<stdarg.h>  
+ 
+#define null NULL  
+#define boolean bool  
+#define True true  
+#define False false  
+#define private /*private*/  
+#define public /*public*/  
+#define Object void*  
+#define Long long long  
+#define infinity 0x7000FFFF  
+#define debugMode false  
+ 
+#define G(T) /*<T>*/  
+  
+  
+void debug(const char* format, ...){  
+    if(debugMode == false)return;  
+    va_list args;  
+    va_start(args, format);  
+    vfprintf(stderr, format, args);  
+    va_end(args);  
+}  
+void endl(){  
+    printf("\n");  
+}  
+/** 
+ * 前面200行基本上都是lib 
+    ⣿⣿⣿⣿⣿⠟⠋⠄⠄⠄⠄⠄⠄⠄⢁⠈⢻⢿⣿⣿⣿⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⡀⠭⢿⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⡟⠄⢀⣾⣿⣿⣿⣷⣶⣿⣷⣶⣶⡆⠄⠄⠄⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⡇⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠄⠄⢸⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⣇⣼⣿⣿⠿⠶⠙⣿⡟⠡⣴⣿⣽⣿⣧⠄⢸⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⣿⣾⣿⣿⣟⣭⣾⣿⣷⣶⣶⣴⣶⣿⣿⢄⣿⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⣿⣿⣿⣿⡟⣩⣿⣿⣿⡏⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⣿⣿⣹⡋⠘⠷⣦⣀⣠⡶⠁⠈⠁⠄⣿⣿⣿⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⣿⣿⣍⠃⣴⣶⡔⠒⠄⣠⢀⠄⠄⠄⡨⣿⣿⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⣿⣿⣿⣦⡘⠿⣷⣿⠿⠟⠃⠄⠄⣠⡇⠈⠻⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⡿⠟⠋⢁⣷⣠⠄⠄⠄⠄⣀⣠⣾⡟⠄⠄⠄⠄⠉⠙⠻ 
+    ⡿⠟⠋⠁⠄⠄⠄⢸⣿⣿⡯⢓⣴⣾⣿⣿⡟⠄⠄⠄⠄⠄⠄⠄⠄ 
+    ⠄⠄⠄⠄⠄⠄⠄⣿⡟⣷⠄⠹⣿⣿⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄ 
+    ⠄⠄⠄⠄⠄⠄⣸⣿⡷⡇⠄⣴⣾⣿⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄ 
+    ⠄⠄⠄⠄⠄⠄⣿⣿⠃⣦⣄⣿⣿⣿⠇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ 
+    ⠄⠄⠄⠄⠄⢸⣿⠗⢈⡶⣷⣿⣿⡏⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄ 
+     
+ */  
+//======================================ArrayList(Vector)========================================================  
+typedef struct ArrayList{  
+    int length;  
+    int maxCapacity;//計算目前陣列是否已經滿了  
+    Object* data;  
+}ArrayList;  
+ArrayList* newArrayList(){  
+    ArrayList* list = malloc(sizeof(ArrayList));  
+    list->length = 0;  
+    list->maxCapacity = 1;  
+    list->data = malloc(list->maxCapacity * sizeof(Object));  
+    return list;  
+}  
+void arrayList_add(ArrayList* list, Object value){  
+    if (list->maxCapacity <= list->length){  
+        list->maxCapacity *= 2;  
+        Object* data = malloc(sizeof(Object) * list->maxCapacity);  
+        for(int i = 0;i < list->length;i++){  
+            data[i] = list->data[i];  
+        }  
+        for(int i = list->length;i < list->maxCapacity;i++){  
+            data[i] = null;  
+        }  
+        //free(list->data);  
+        list->data = data;  
+    }  
+    list->data[list->length] = value;  
+    list->length++;  
+}  
+Object arrayList_get(ArrayList* list, int index){  
+    assert(0<=index && index <list->length);  
+    return list->data[index];  
+}  
+Object arrayList_peek(ArrayList* list){  
+    return arrayList_get(list, list->length-1);  
+}  
+Object arrayList_pop(ArrayList* list){  
+    Object obj = arrayList_peek(list);  
+    list->length--;  
+    return obj;  
+}  
+void arrayList_sort(ArrayList* list, int(*compareFunc)(const Object o1, const Object o2)){  
+    //debug("arrayList.sort()\n");  
+    //qsort(list->data, list->length, sizeof(Object), compareFunc);  
+      
+    int len = list->length;  
+    for(int i = 0;i<len;i++){  
+        int count = 0;  
+        for(int j = 0;j<len-i-1;j++){  
+            //debug("compare [%d] and [%d]\n", j, j+1);  
+            int cmp = compareFunc(list->data[j], list->data[j+1]);  
+            //debug("cmp=%lf\n", cmp);  
+            if(cmp > 0){  
+                //debug("swap\n");  
+                Object temp = list->data[j];  
+                list->data[j] = list->data[j+1];  
+                list->data[j+1] = temp;  
+                count++;  
+            }  
+            //debug("\n");  
+        }  
+        if(count==0)break;  
+    }  
+    //debug("sort complete\n");  
+}  
+boolean arrayList_contain(ArrayList* list, Object value){  
+    int len = list->length;  
+    for(int i = 0;i<len;i++){  
+        if(list->data[i] == value)return true;  
+    }  
+    return false;  
+}  
+//================================================DequeNode========================================================  
+struct DequeNode{  
+    struct DequeNode* next;  
+    struct DequeNode* previous;  
+    Object value;  
+};  
+typedef struct DequeNode DequeNode;  
+DequeNode* newDequeNode(Object value){  
+    DequeNode* node = malloc(sizeof(DequeNode));  
+    node->next = null;  
+    node->previous = null;  
+    node->value = value;  
+    return node;  
+}  
+//=================================================Stack==========================================================  
+typedef struct Stack{  
+    DequeNode* head;  
+    DequeNode* tail;  
+    int size;  
+}Stack;  
+Stack* newStack(){  
+    Stack* stack = malloc(sizeof(Stack));  
+    stack->head = null;  
+    stack->tail = null;  
+    stack->size = 0;  
+    return stack;  
+}  
+boolean stack_empty(Stack* stack){  
+    return stack->size == 0;  
+}  
+  
+void stack_add(Stack* stack, Object value){  
+    assert(value != null);  
+    DequeNode* node = newDequeNode(value);  
+    if(stack_empty(stack)){  
+        stack->head = node;  
+        stack->tail = node;  
+    }else{  
+        stack->tail->next = node;  
+        node->previous = stack->tail;  
+        stack->tail = node;  
+    }  
+    stack->size++;  
+      
+}  
+Object stack_peek(Stack* stack){  
+    assert(stack_empty(stack)==false);  
+    return stack->tail->value;  
+}  
+Object stack_pop(Stack* stack){  
+      
+    assert(stack_empty(stack)==false);  
+    Object value = stack->tail->value;  
+    assert(value != null);  
+    if(stack->size == 1){  
+        stack->tail = null;  
+        stack->head = null;  
+    }else{  
+        stack->tail = stack->tail->previous;  
+        stack->tail->next = null;  
+    }  
+    stack->size--;  
+    //debug("pop complete\n");  
+    return value;  
+}  
+//==========================================Queue=================================================================  
+typedef struct Queue{  
+    DequeNode* head;  
+    DequeNode* tail;  
+    int size;  
+}Queue;  
+Queue* newQueue(){  
+    Queue* queue = malloc(sizeof(Queue));  
+    queue->head = null;  
+    queue->tail = null;  
+    queue->size = 0;  
+    return queue;  
+}  
+boolean queue_empty(Queue* queue){  
+    return queue->size == 0;  
+}  
+  
+void queue_add(Queue* queue, Object value){  
+    DequeNode* node = newDequeNode(value);  
+    if(queue_empty(queue)){  
+        queue->head = node;  
+        queue->tail = node;  
+    }else{  
+        queue->tail->next = node;  
+        node->previous = queue->tail;  
+        queue->tail = node;  
+    }  
+    queue->size++;  
+}  
+Object queue_peek(Queue* queue){  
+    assert(queue_empty(queue)==false);  
+    return queue->head->value;  
+}  
+Object queue_pop(Queue* queue){  
+    assert(queue_empty(queue)==false);  
+    DequeNode* node = queue->head;  
+    if(queue->size == 1){  
+        queue->tail = null;  
+        queue->head = null;  
+    }else{  
+        queue->head = queue->head->next;  
+        queue->head->previous = null;  
+    }  
+    queue->size--;  
+    return node->value;  
+}  
+  
+  
+//=============================================================================================  
+  
+//題目 : https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30787
+
+boolean checkDiagonal(int map[3][3]){
+    return (map[0][0] == map[1][1] && map[1][1] == map[2][2]) || (map[2][0] == map[1][1] && map[1][1] == map[0][2]);
+}
+boolean checkVertical(int map[3][3]){
+    if(map[0][0] == map[1][0] && map[1][0] == map[2][0])return true;
+    if(map[0][1] == map[1][1] && map[1][1] == map[2][1])return true;
+    if(map[0][2] == map[1][2] && map[1][2] == map[2][2])return true;
+    return false;
+}
+boolean checkHorizontal(int map[3][3]){
+    if(map[0][0] == map[0][1] && map[0][1] == map[0][2])return true;
+    if(map[1][0] == map[1][1] && map[1][1] == map[1][2])return true;
+    if(map[2][0] == map[2][1] && map[2][1] == map[2][2])return true;
+    return false;
+
+}
+
+int main(){  
+    int map[3][3];
+    for(int i = 0;i<3;i++){
+        for(int j = 0;j<3;j++){
+            scanf("%d", &(map[i][j]));
+        }
+    }
+    boolean succ = checkDiagonal(map) || checkVertical(map) || checkHorizontal(map);
+    printf("%s\n", succ ? "True" : "False");
+    return 0;  
+}  
